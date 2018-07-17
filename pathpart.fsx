2a3e5ee61d173ee3bc109d943ba33f8ad239ca8a
Stephane Larocque <stephl001@gmail.com>
2018-07-17 15:56:42 -0400

    Full implementation

--- pathpart.fsx
+++ pathpart.fsx
@@ -1,36 +1,46 @@
-#r "C:/Users/ratat/Projects/pathpart/packages/Newtonsoft.Json/lib/net45/Newtonsoft.Json.dll"
+#r "packages/Newtonsoft.Json/lib/net45/Newtonsoft.Json.dll"
 open Newtonsoft.Json.Linq
-
 open System
 open System.Text.RegularExpressions
 
+type PathValue = string*Object
+
 module PathPart = 
-    let (|CollectionProperty|_|) part =
+    let private (|CollectionProperty|_|) part =
         let m = Regex.Match(part, @"^([^\[]+)\[0\]$")
         if m.Success then Some m.Groups.[1].Value else None
-    let (|PropertyName|_|) (part:string) =
+    let private (|PropertyName|_|) (part:string) =
         if Regex.IsMatch(part, @"^[a-zA-Z][a-zA-Z0-9_]*$")
         then Some part
         else None
 
-    let split (s:string) = s.Split('.') |> List.ofArray
+    let private jsonProperty name (value:Object) =
+        new JObject(new JProperty(name, value))
 
-    let private jsonProperty name (value:Object) : JToken =
-        new JObject(new JProperty(name, value)) :> _
+    let private jsonCollection name (value:Object) =
+        jsonProperty name (new JArray([|value|]))
 
-    let private jsonCollection name (value:Object) : JToken =
-        new JArray(jsonProperty name value) :> _
-
-    let rec toJson (value:Object)  = function
+    let rec private toJson (value:Object)  = function
     | [CollectionProperty p] -> jsonCollection p value
     | [PropertyName p] -> jsonProperty p value
     | (CollectionProperty p)::rest -> toJson value rest |> jsonCollection p 
     | (PropertyName p)::rest -> toJson value rest |> jsonProperty p
     | _ -> failwith "Oups!"
 
-    let convertPath value = split >> toJson value
+    let private toStringParts (path:string) = path.Split [|'.'|] |> List.ofArray
+    let private convertPath (p,v) = p |> toStringParts |> toJson v
 
-    let merge (o1:JObject) (o2:JObject) = o1.Merge(o2); o1
+    let private merge (o1:JObject) (o2:JObject) = 
+        o1.Merge(o2, new JsonMergeSettings(MergeArrayHandling = MergeArrayHandling.Merge))
+        o1
 
-    let mergeAll (objs:JObject list) =
-        objs |> List.fold merge (new JObject())
+    let private getDuplicatePaths = 
+        List.groupBy fst >> List.filter (snd >> List.length >> (<>) 1) >> List.map fst
+    let private validatePathValues (pv : PathValue list) = 
+        let duplicates = getDuplicatePaths pv
+        match duplicates with
+        | [] -> pv
+        | _ -> failwith (sprintf "The following keys were duplicated with different values: %A" duplicates)
+
+    let private mergeAll = new JObject() |> List.fold merge
+    let handlePathValueList = validatePathValues >> List.map convertPath >> mergeAll
